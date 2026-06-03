4a05fb155cfacd944b7dd513dee8b4c37000adab
alshakliamohammed4-bit <alshakliamohammed4@gmail.com>
2026-06-03 08:48:03 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,47 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getFirestore,
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+doc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+getAuth,
+signInWithEmailAndPassword,
+createUserWithEmailAndPassword,
+signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDT_mbvSi516vuugJKi2xbrWmhr_5-Gqag",
+  authDomain: "school-system-qaflah.firebaseapp.com",
+  projectId: "school-system-qaflah",
+  storageBucket: "school-system-qaflah.firebasestorage.app",
+  messagingSenderId: "469860732094",
+  appId: "1:469860732094:web:21869dd6e6d018c1cbe2ef",
+  measurementId: "G-P0RVPRJ454"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export {
+db,
+auth,
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+doc,
+signInWithEmailAndPassword,
+createUserWithEmailAndPassword,
+signOut
+};
