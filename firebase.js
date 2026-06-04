a416cdc09986e5637376980d023c68a6805804db
alshakliamohammed4-bit <alshakliamohammed4@gmail.com>
2026-06-04 17:14:08 +0300

    Refactor Firebase imports and export structure

--- firebase.js
+++ firebase.js
@@ -1,22 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-getAuth,
-signInWithEmailAndPassword,
-onAuthStateChanged,
-signOut
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import {
-getFirestore,
-collection,
-addDoc,
-getDocs,
-deleteDoc,
-doc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-/* 🔥 Firebase Config (نفس بياناتك) */
 const firebaseConfig = {
 apiKey: "AIzaSyDT_mbvSi516vuugJKi2xbrWmhr_5-Gqag",
 authDomain: "school-system-qaflah.firebaseapp.com",
@@ -26,16 +14,11 @@ messagingSenderId: "469860732094",
 appId: "1:469860732094:web:21869dd6e6d018c1cbe2ef"
 };
 
-/* تشغيل Firebase */
 const app = initializeApp(firebaseConfig);
 
-/* Auth */
 export const auth = getAuth(app);
-
-/* Firestore */
 export const db = getFirestore(app);
 
-/* أدوات عامة */
 export {
 signInWithEmailAndPassword,
 onAuthStateChanged,
